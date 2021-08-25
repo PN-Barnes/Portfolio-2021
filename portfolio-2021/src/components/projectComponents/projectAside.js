ff267b4b763a59a4696297c81e5189a99b880793
Paul N Barnes <paulieb99@gmail.com>
2021-08-24 18:20:46 -0700

    listed jamMap component

---
+++ portfolio-2021/src/components/projectComponents/projectAside.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function projectAside() {
+  return (
+    <aside class="projectsContainer container-fluid mt-5 mb-5">
+      <jamMap />
+    </aside>
+  );
+}
+
+export default projectAside;
