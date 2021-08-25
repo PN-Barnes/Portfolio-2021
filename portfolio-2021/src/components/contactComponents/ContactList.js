af525a5b3fcb71095638fb2dfa0a18ddcd8d8a54
Paul N Barnes <paulieb99@gmail.com>
2021-08-24 19:11:36 -0700

    added github component

--- portfolio-2021/src/components/contactComponents/ContactList.js
+++ portfolio-2021/src/components/contactComponents/ContactList.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Email from './navComponents/Email';
 import LinkedIn from './navComponents/LinkedIn';
+import Github from './navComponents/Github';
+
 export default function ContactList() {
   return (
     <ul class="nav justify-content-center nav-pills">
       <Email />
       <LinkedIn />
+      <Github />
     </ul>
   );
 }
---
+++ portfolio-2021/src/components/contactComponents/navComponents/Github.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Github() {
+  return (
+    <li>
+      <a class="nav-link" href="https://github.com/PN-Barnes">
+        Github
+      </a>
+    </li>
+  );
+}
