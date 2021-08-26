7af5a8084a617e0584ba3368ba427970944ae61e
Paul N Barnes <paulieb99@gmail.com>
2021-08-26 13:03:49 -0700

    styled home page better

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer className="h5 justify-content-center text-center footer bg-dark">
+    <footer className="h5 justify-content-center text-center footer bg-dark p-3">
       <h3>Made with Bootstrap, React, and Custom CSS</h3>
     </footer>
   );
--- src/components/Portrait.js
+++ src/components/Portrait.js
@@ -8,29 +8,32 @@ const borderStyle = {
 };
 export default function Portrait() {
   return (
-    <div
-      className="card container-fluid portrait justify-content-center mt-6"
-      id="portrait-container"
-    >
-      <img
-        src={tierraAlta}
-        className="card-img img-fluid"
-        id="tierraAlta"
-        alt=""
-      />
-      <div className="card-img-overlay">
-        {/* <img
-          className="img-fluid img-thumbnail tierra"
-          src={pbPic}
-          // eslint-disable-next-line react/jsx-no-duplicate-props
-          width="200px"
-          height="200px"
-          style={borderStyle.border}
-          alt="portrait of me!"
-        /> */}
-        <h1 className="h1 text-center hello">Hello World!</h1>
-        <h1 className="h1 text-center paul">Paul Barnes</h1>
-        <h1 className="h1 text-center fullstack">FullStack Developer</h1>
+    <div>
+      <h1 className="h1 text-center hello">Hello World!</h1>
+      <div
+        className="card container-fluid portrait justify-content-center mt-6"
+        id="portrait-container"
+      >
+        <img
+          src={tierraAlta}
+          className="card-img img-fluid"
+          id="tierraAlta"
+          alt=""
+        />
+        <div className="card-img-overlay">
+          <img
+            className="img-fluid img-thumbnail tierra"
+            src={pbPic}
+            // eslint-disable-next-line react/jsx-no-duplicate-props
+            width="200px"
+            height="200px"
+            style={borderStyle.border}
+            alt="portrait of me!"
+          />
+
+          <h1 className="h1 text-center paul">Paul Barnes</h1>
+          <h1 className="h1 text-center fullstack">FullStack Developer</h1>
+        </div>
       </div>
     </div>
   );
