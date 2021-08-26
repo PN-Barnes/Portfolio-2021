8f000437cd2c50c8a5f7030ccdfc123fefd21b91
Paul N Barnes <paulieb99@gmail.com>
2021-08-26 10:49:20 -0700

    fixed image for correct dimensions

--- src/components/Portrait.js
+++ src/components/Portrait.js
@@ -9,18 +9,23 @@ const borderStyle = {
 export default function Portrait() {
   return (
     <div
-      className="card container-lg portrait justify-content-center mt-6"
+      className="card container-fluid portrait justify-content-center mt-6"
       id="portrait-container"
     >
-      <img src={tierraAlta} className="card-img" id="tierraAlta" alt="" />
+      <img
+        src={tierraAlta}
+        className="card-img img-fluid"
+        id="tierraAlta"
+        alt=""
+      />
       <div className="card-img-overlay">
         <img
-          className="img-fluid rounded mx-auto d-block img-thumbnail"
+          className="img-fluid"
           src={pbPic}
           // eslint-disable-next-line react/jsx-no-duplicate-props
           className="headshot"
           width="200px"
-          height="300px"
+          height="200px"
           style={borderStyle.border}
           alt="portrait of me!"
         />
