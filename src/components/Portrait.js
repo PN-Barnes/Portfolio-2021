7a8904947f6dc88c11ded848ae52f59c8456902c
Paul N Barnes <paulieb99@gmail.com>
2021-08-26 18:36:55 -0700

    added about me section

--- src/components/Portrait.js
+++ src/components/Portrait.js
@@ -41,6 +41,29 @@ export default function Portrait() {
           <h1 className="h1 text-center fullstack">FullStack Developer</h1>
         </div>
       </div>
+      <div className="container-md ">
+        <h1>About Me</h1>
+        <p className="text-left">
+          Hello! Welcome to my portfolio page. Some facts about this site,
+          first, everything you see on this site was created by me. Some of the
+          technologies used here are React, the Bootstrap framework, and some
+          custom css.
+          <br />
+          <br />
+          If you're interested in checking out the structure of this site,
+          checkout the github repository listed below. Otherwise, checkout my
+          projects, they are a clear example of my skills and hope you find them
+          interesting, for a more in depth look at my skills and background,
+          checkout the bio page to see the list of skills, training, and resume
+          upated through 2021.
+          <br />
+          <br />
+          If you have any questions, head over to the contact page and fill out
+          the form to reach me directly, Otherwise, my Email and online profiles
+          are listed above the footer. Thank you for visiting my site and look
+          forward to connecting with you.
+        </p>
+      </div>
     </div>
   );
 }
