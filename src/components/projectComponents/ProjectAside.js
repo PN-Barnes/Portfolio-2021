e0e82f44c3d5d9ec515ea2f648cc11fcbf66b9c5
Paul N Barnes <paulieb99@gmail.com>
2021-08-29 14:58:20 -0700

    Created project objects and stored them within the project array

--- src/components/projectComponents/ProjectAside.js
+++ src/components/projectComponents/ProjectAside.js
@@ -10,8 +10,34 @@ export default function ProjectAside() {
   const project = [
     {
       projectName: 'Jam-Map',
-      github: '',
+      github: 'https://github.com/PDPco/jam-map',
+      deployed: 'https://pdpco.github.io/jam-map/',
     },
+    {
+      projectName: 'Clove',
+      github: 'https://github.com/PN-Barnes/CLOVE',
+      deployed: 'https://still-reef-24172.herokuapp.com/'
+    },
+    {
+      projectName: 'Company Organizer',
+      github: 'https://github.com/PN-Barnes/E-commerce',
+      deployed: 'https://github.com/PN-Barnes/E-commerce'
+    }
+    {
+      projectName: 'Weather Dashboard',
+      github: 'https://github.com/PN-Barnes/weather-dashboard',
+      deployed: 'https://pn-barnes.github.io/weather-dashboard/'
+    }
+    {
+      projectName: 'Workday Scheduler',
+      github: 'https://github.com/PN-Barnes/WorkDay-Scheduler',
+      deployed: 'https://pn-barnes.github.io/WorkDay-Scheduler/'
+    }
+    {
+      projectName: 'Password Generator',
+      github: 'https://github.com/PN-Barnes/random-password-generator',
+      deployed: 'https://pn-barnes.github.io/random-password-generator/'
+    }
   ];
   return (
     <aside className="projectsContainer container-fluid mt-5 mb-5">
